9feaff6fc9e3c4852e6b01a50b7f9c3bfceaec95
Emartinez511 <mrblonde9993@gmail.com>
2016-05-06 10:05:14 -0500

    This is a seeder to fill in the albums table.

---
+++ albums_seeder.sql
@@ -0,0 +1,13 @@
+ USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop, rock, R&B'),
+('AC/DC', 'Back in Black', 1980, 25.9, 'Hard rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'Soundtrack/R&B'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock'),
+('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 19, 'Disco'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+('Shania Twain', 'Come On Over', 1997, 29.6, 'Country'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard Rock');
