b79963daa491cec76d0c77d936857f8b13db466d
Emartinez511 <mrblonde9993@gmail.com>
2016-05-09 11:15:58 -0500

    This was an exercise using group by

---
+++ group_by_exercise.sql
@@ -0,0 +1,16 @@
+USE titles;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY first_name, last_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'   
+GROUP BY last_name;
